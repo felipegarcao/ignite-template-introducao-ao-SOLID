2de74d6e1bda038c28f3afd26882ad1417d8bd74
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-11-02 16:29:23 -0300

    Desafio Concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,22 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id?: string;
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error('Only admin can access the list')
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
